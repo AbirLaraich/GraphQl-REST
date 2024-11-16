d524bacaff53538627fedfc7ee342648e5d79d9d
la233225 <abir.laraich@etu.univ-lehavre.fr>
2024-11-16 03:38:57 +0100

    fix: ajouter les enumerations

--- models/AnnonceModel.js
+++ models/AnnonceModel.js
@@ -1,13 +1,29 @@
 const mongoose = require('mongoose');
 
+const TypeBienEnum = ['vente', 'location'];
+const StatutPublicationEnum = ['publiee', 'non_publiee'];
+const StatutBienEnum = ['disponible', 'loue', 'vendu'];
+
 const annonceSchema = new mongoose.Schema({
   titre: { type: String, required: true },
   description: String,
   prix: Number,
-  statutPublication: String,
+  statutPublication: { 
+    type: String, 
+    enum: StatutPublicationEnum, 
+    required: true 
+  },
   dateDisponibilite: Date,
-  typeBien: String,
-  statutBien: String,
+  typeBien: { 
+    type: String, 
+    enum: TypeBienEnum, 
+    required: true 
+  },
+  statutBien: { 
+    type: String, 
+    enum: StatutBienEnum, 
+    required: true 
+  },
   photos: [String],
   questions: [
     {
@@ -17,7 +33,7 @@ const annonceSchema = new mongoose.Schema({
         {
           contenu: { type: String },
           dateReponse: { type: Date, default: Date.now },
-          reponduPar: { type: mongoose.Schema.Types.ObjectId, ref: 'User' } 
+          reponduPar: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
         }
       ]
     }
