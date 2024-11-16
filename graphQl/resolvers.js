c8fed3c81c0c63ccb9903accb89ada2369ee56e7
la233225 <abir.laraich@etu.univ-lehavre.fr>
2024-11-16 03:41:57 +0100

    fix: corriger les anomalies d'enumration

--- graphQl/resolvers.js
+++ graphQl/resolvers.js
@@ -1,50 +1,79 @@
-const Annonce = require('../models/AnnonceModel'); 
-const User = require('../models/UserModel'); 
+const AnnonceService = require("../service/AnnoncesService");
+const UserService = require("../service/UsersService");
 
 const resolvers = {
-  Query: {
-    annonces: async (_, { typeBien, statutBien, prixMin, prixMax }) => {
-      const filter = {};
-      if (typeBien) filter.typeBien = typeBien;
-      if (statutBien) filter.statutBien = statutBien;
-      if (prixMin !== undefined || prixMax !== undefined) {
-        filter.prix = {};
-        if (prixMin !== undefined) filter.prix.$gte = prixMin;
-        if (prixMax !== undefined) filter.prix.$lte = prixMax;
-      }
-      return await Annonce.find(filter);
+    Query: {
+      annonces: async (_, args) => {
+        const { typeBien, statutBien, statutPublication, prixMin, prixMax } = args;
+        let annonces = await AnnonceService.getAllAnnonces();
+        
+        if (prixMin !== undefined) {
+          annonces = annonces.filter((a) => a.prix >= prixMin);
+        }
+        if (prixMax !== undefined) {
+          annonces = annonces.filter((a) => a.prix <= prixMax);
+        }
+        return annonces;
+      },
+  
+      annonce: async (_, { id }) => {
+        return await AnnonceService.getAnnonceById(id);
+      },
+  
+      questions: async (_, { annonceId }) => {
+        return await AnnonceService.getQuestionsByAnnonceId(annonceId);
+      },
+  
+      user: async (_, { email }) => {
+        return await UserService.getUserByEmail(email);
+      },
     },
-    annonce: async (_, { id }) => await Annonce.findById(id),
-    questions: async (_, { annonceId }) => {
-      const annonce = await Annonce.findById(annonceId);
-      return annonce ? annonce.questions : [];
+    Mutation: {
+      createAnnonce: async (_, { input }, context) => {
+        if (!context.user || context.user.role !== "AGENT") {
+          throw new Error("Seuls les agents peuvent créer des annonces");
+        }
+        return await AnnonceService.createAnnonce(input);
+      },
+  
+      updateAnnonce: async (_, { id, input }, context) => {
+        if (!context.user || context.user.role !== "AGENT") {
+          throw new Error("Seuls les agents peuvent modifier des annonces");
+        }
+        return await AnnonceService.updateAnnonce(input, id);
+      },
+  
+      deleteAnnonce: async (_, { id }, context) => {
+        if (!context.user || context.user.role !== "AGENT") {
+          throw new Error("Seuls les agents peuvent supprimer des annonces");
+        }
+        await AnnonceService.deleteAnnonce(id);
+        return true;
+      },
+  
+      addQuestion: async (_, { annonceId, input }, context) => {
+        if (!context.user) {
+          throw new Error("Vous devez être connecté pour poser une question");
+        }
+        return await AnnonceService.addQuestion(annonceId, input.contenu);
+      },
+  
+      addReponse: async (_, { annonceId, questionId, input }, context) => {
+        if (!context.user || context.user.role !== "AGENT") {
+          throw new Error("Seuls les agents peuvent répondre aux questions");
+        }
+        return await AnnonceService.addReponse(
+          annonceId,
+          questionId,
+          input.contenu,
+          context.user.id
+        );
+      },
+  
+      createUser: async (_, { email, name, password, role }) => {
+        return await UserService.createUser({ email, name, password, role });
+      },
     },
-    user: async (_, { email }) => await User.findOne({ email }),
-  },
-  Mutation: {
-    createAnnonce: async (_, { input }) => await Annonce.create(input),
-    updateAnnonce: async (_, { id, input }) => await Annonce.findByIdAndUpdate(id, input, { new: true }),
-    deleteAnnonce: async (_, { id }) => {
-      await Annonce.findByIdAndDelete(id);
-      return true;
-    },
-    addQuestion: async (_, { annonceId, input }) => {
-      const annonce = await Annonce.findById(annonceId);
-      annonce.questions.push(input);
-      await annonce.save();
-      return annonce.questions[annonce.questions.length - 1];
-    },
-    addReponse: async (_, { annonceId, questionId, input }) => {
-      const annonce = await Annonce.findById(annonceId);
-      const question = annonce.questions.id(questionId);
-      question.reponses.push(input);
-      await annonce.save();
-      return question.reponses[question.reponses.length - 1];
-    },
-    createUser: async (_, { email, name, password, role }) => {
-      return await User.create({ email, name, password, role });
-    },
-  },
-};
-
-module.exports = resolvers;
+  };
+  
+  module.exports = resolvers;
